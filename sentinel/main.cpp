370e60446c0d3c7eac28e300701a398f09e26055
John Carter <johngallweycarter@gmail.com>
2016-11-13 17:27:18 -0500

    rawr

---
+++ sentinel/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ifstream fin("data.dat");
+	ofstream fout("report.txt");
+	int counter = 0;
+	int total = 0;
+
+	if(fin.fail())
+	{
+		cerr << "Could not open file";
+		exit(1);
+	}
+
+	if(fout.fail())
+	{
+		cerr << "Could not open output file";
+		exit(1);
+	}
+
+	string data;
+
+	while(data != "-99"){ 
+		getline(fin, data);
+		int rawr = atoi(data.c_str());
+		if(rawr != -99){
+			total += rawr;
+			counter++;
+		}
+
+	}
+
+	float avg = total / counter;
+	
+
+	fout << counter << " Counter" << endl;
+	fout << total << " Total" << endl;
+	fout << avg << " Average" << endl;
+	
+	return 0;
+}
+
